e138c61febf880b1dd98d9ca1bac1c46aee8621e
Teyi Daniel Lawson <danylaws@gmail.com>
2023-07-21 09:29:22 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{name: 'home'}">Todos</router-link>
+    <router-link :to="{name: 'categories'}">Categories</router-link>
   </nav>
   <router-view/>
 </template>
@@ -11,8 +11,9 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  width :80%;
+  margin: 0 auto;
 }
 
 nav {
@@ -20,11 +21,15 @@ nav {
 }
 
 nav a {
+  
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 8px 12px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background-color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="home">
+    
+    <div>
+      <button>Create Todo</button>
+    </div>
+    <div v-for="todo in todos" :key="todo.id">
+      <div class="todo" :class="{done: todo.isDone}">
+        <div class="title">{{ todo.title }}</div>
+        <div class="schedulefor">Scheduled for: {{ todo.scheduledFor }}</div>
+        <div class="actions">
+          <span class="edit">Edit</span>
+          <span class="delete">delete</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {},
+  setup(){
+    
+    let todos = ref([ 
+      {"id": 1,"title": "Learn Vue","scheduledFor": "07/28/2023","isDone": false},
+      {"id": 2, "title": "Wash dishes","scheduledFor": "07/25/2023","isDone": true}
+    ])
+
+    return {todos}
+  }
+}
+</script>
+
+<style>
+
+.todo {
+  display:flex; 
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 10px;
+  padding: 8px 12px;
+  margin : 10px 0;
+  background-color:rgb(190, 190, 190);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.done {
+  color: green;
+  background-color:rgb(198, 240, 198)
+}
+
+
+/* CSS for the action buttons */
+.todo .actions {
+  display: flex;
+  gap: 10px;
+}
+
+/* Styling for the edit and delete buttons */
+.edit, .delete {
+  padding: 8px 12px;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+}
+
+/* Edit button styling */
+.edit {
+  background-color: #4caf50;
+}
+
+/* Delete button styling */
+.delete {
+  background-color: #f44336;
+}
+
+</style>
